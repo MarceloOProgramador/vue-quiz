9b6fdb4ac13bb4daae1bb6673a8355e0704f41f0
Marcelo Pereira <marcelopereiraIFSP@gmail.com>
2018-08-20 18:56:54 -0300

    First commit

---
+++ src/Components/Animations/Question.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2>{{ask}}</h2>
+    <form @submit.prevent="checkResult">
+      <input type="text" name="answer" id="answer" placeholder="Resultado" v-model="reply"/>
+      <button>Responder</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  created(){
+    this.generateQuestion();
+  },
+  data ()  {
+    return {
+      ask : "Gerando pergunta...",
+      result : 0,
+      reply : ""
+    }
+  },
+  methods : {
+
+    generateQuestion() {
+
+      let numberOne = this.generateRandomNumber(1, 100);
+
+      let numberTwo = this.generateRandomNumber(1, 100);
+
+      this.result = numberOne + numberTwo;
+
+      this.ask = numberOne + ' + ' + numberTwo + ' = ' + '?';
+    },
+    generateRandomNumber (min, max){
+
+      return Math.round(Math.random() * (max - min) + min);
+
+    },
+    checkResult () {
+
+      if (this.reply == this.result) {
+
+        alert("Acertou!!");
+
+        this.generateQuestion();
+
+        this.reply = "";
+
+      } else {
+
+        alert("Errou!");
+
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Components/Animations/Quiz.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 v-text="title"></h2>
+    <question-component></question-component>
+  </div>
+</template>
+
+<script>
+import Question from "./Question"
+export default {
+  data (){
+    return {
+      title: "Quiz"
+    }
+  },
+  components: {
+    "question-component" : Question
+  }
+}
+</script>
+  
+<style scoped>
+
+</style>
